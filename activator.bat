a6920d06341ebd8ce61ebecf1c328852c6028e74
Anders Hagelin Abrahamsen <anders@amfiet.no>
2016-05-11 14:11:37 +0200

    Further reduce mem

--- activator.bat
+++ activator.bat
@@ -151,7 +151,7 @@ for /f "delims=. tokens=1-3" %%v in ("%JAVA_VERSION%") do (
  )
 
 rem We use the value of the JAVA_OPTS environment variable if defined, rather than the config.
-set _JAVA_OPTS=%JAVA_OPTS% -Xms512m -Xmx1536m -Xss1m
+set _JAVA_OPTS=%JAVA_OPTS% -Xms256m -Xmx512m -Xss1m
 
 set DEBUG_OPTS=
 
